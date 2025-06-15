3f1a098c68d07e569c44a208a088d3c2af14049d
Lucas Calabria <lucascalabria86@gmail.com>
2025-06-15 14:05:39 -0300

    header

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import Navbar from '@/components/Navbar';
+import Header from '@/components/dashboard/Header';
 import Sidebar from '@/components/Sidebar';
 import ProtectedRoute from '@/components/ProtectedRoute';
 
@@ -13,7 +13,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
       <div className="min-h-screen bg-dark-800 flex">
         <Sidebar />
         <div className="flex-1 flex flex-col overflow-hidden">
-          <Navbar />
+          <Header />
           <main className="flex-1 overflow-y-auto px-4 py-8 sm:px-6 lg:px-8">
             <div className="max-w-7xl mx-auto">
               {children}
